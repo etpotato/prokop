73c56e88727e220e8c7381c380fe1048afaa0c5c
etpotato <mareev.pv@gmail.com>
2021-05-18 22:47:37 +0300

    Add webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,11 @@
 module.exports = {
   mode: 'production',
   entry: {
-    index: ['./source/js/menu.js', './source/js/header-region.js', './source/js/form.js'],
-    procedure: ['./source/js/menu.js', './source/js/procedure-tabs.js', './source/js/form.js', './source/js/carousel.js'],
-    contact: ['./source/js/menu.js', './source/js/header-region.js'],
-    blank: ['./source/js/menu.js', './source/js/header-region.js', './source/js/blank.js'],
+    index: [
+      './source/js/carousel.js',
+      './source/js/api.js',
+      './source/js/form.js',
+    ],
   },
   devtool: 'source-map',
   output: {
